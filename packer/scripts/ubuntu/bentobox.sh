e0cfff05c168b0403e65b839bbddc219301120f6
Sirio Marchi <sirio.marchi@gmail.com>
2014-06-10 17:43:39 +0200

    added more packages to provisioning; fixed names;

--- packer/scripts/ubuntu/bentobox.sh
+++ packer/scripts/ubuntu/bentobox.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -eux
 
+# silent LAMP installation
 echo "mysql-server mysql-server/root_password password root" | debconf-set-selections
 echo "mysql-server mysql-server/root_password_again password root" | debconf-set-selections
 echo 'phpmyadmin phpmyadmin/dbconfig-install boolean true' | debconf-set-selections
@@ -10,6 +11,21 @@ echo 'phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2' | debconf
 
 apt-get install -y lamp-server^
 apt-get install -y phpmyadmin
+apt-get install -y apache2-utils
+
+# install common tools
 apt-get install -y vim
 apt-get install -y mc
+apt-get install -y htop
 
+# disable passing local env throught ssh
+sed -i 's/\(AcceptEnv LANG LC_\*\)/#\1/' /etc/ssh/sshd_config
+
+# install GCC to build VBoxGuestAdditions
+# apt-get install -y build-essential
+
+# fix VBoxGuestAdditions path
+ln -s /opt/VBoxGuestAdditions-*/lib/VBoxGuestAdditions /usr/lib/VBoxGuestAdditions
+
+# install LXDE
+apt-get install lubuntu-desktop
